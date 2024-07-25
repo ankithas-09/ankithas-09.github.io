c9471b86179f93bf97900ef9042783986ecfa2d5
Ankitha S <ankithasures@umass.edu>
2024-07-25 14:35:20 +0530

    Updated Work Company Name

--- js/custom.js
+++ js/custom.js
@@ -155,7 +155,7 @@
         if ($('.fullpage-default').length) {
             var myFullpage = new fullpage('.fullpage-default', {
                 licenseKey: ' C7F41B00-5E824594-9A5EFB99-B556A3D5',
-                anchors: ['slide01', 'slide02', 'slide03', 'slide04', 'slide05', 'slide06', 'slide07'],
+                anchors: ['homepage', 'about', 'skills', 'experience', 'project', 'contact'],
                 menu: '#nav',
                 lazyLoad: true,
                 navigation: true,
